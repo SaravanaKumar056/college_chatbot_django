1909c7c5800189f80afda4d216269e0aca42bb2e
SaravanaKumar056 <23bai056@hicas.ac.in>
2025-04-09 19:18:35 +0530

    Initial commit - Django chatbot project

--- chatbot-env/Scripts/activate.bat
+++ chatbot-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Saravanakumar\OneDrive\Desktop\google colab\college_chatbot\college_chatbot\college_chatbot\college_chatbot_django\chatbot-env
+set "VIRTUAL_ENV=C:\Users\Saravanakumar\OneDrive\Desktop\google colab\college_chatbot_django\chatbot-env"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(chatbot-env) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(chatbot-env) "
 
 :END
 if defined _OLD_CODEPAGE (
